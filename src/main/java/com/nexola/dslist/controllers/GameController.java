a3f07c5eba91d099602fad1df30f212205f2cc16
Vitor Vianna <vitormatheusfv@gmail.com>
2023-05-23 17:58:19 -0300

    GET game by id

--- src/main/java/com/nexola/dslist/controllers/GameController.java
+++ src/main/java/com/nexola/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.nexola.dslist.controllers;
 
+import com.nexola.dslist.dto.GameDTO;
 import com.nexola.dslist.dto.GameMinDTO;
 import com.nexola.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- src/main/java/com/nexola/dslist/services/GameService.java
+++ src/main/java/com/nexola/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.nexola.dslist.services;
 
+import com.nexola.dslist.dto.GameDTO;
 import com.nexola.dslist.dto.GameMinDTO;
 import com.nexola.dslist.entities.Game;
 import com.nexola.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired // Injeta o componente repository ao componente service
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true) // Assegura que nenhuma solicitação de escrita será feita
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
