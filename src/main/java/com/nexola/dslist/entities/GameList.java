c165cdc3cda6170826c881e6edc6f022306552e2
Vitor Vianna <vitormatheusfv@gmail.com>
2023-05-23 17:29:21 -0300

    Database seed update

--- src/main/java/com/nexola/dslist/entities/GameList.java
+++ src/main/java/com/nexola/dslist/entities/GameList.java
@@ -1,16 +1,15 @@
 package com.nexola.dslist.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_gamelist")
+@Table(name = "tb_game_list")
 public class GameList {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
