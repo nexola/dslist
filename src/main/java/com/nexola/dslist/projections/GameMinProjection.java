e97556d247cdfed89f7660d0419dab68bd89d78c
Vitor Vianna <vitormatheusfv@gmail.com>
2023-05-25 11:16:31 -0300

    Homolog ok

--- src/main/java/com/nexola/dslist/projections/GameMinProjection.java
+++ src/main/java/com/nexola/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ public interface GameMinProjection { // Projection precisa ter as propriedades r
 
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Long getPosition();
